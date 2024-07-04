463ead80172d7e8e530d9869b754ff5c381b3c49
abdulrahman <mrabdulrahman95@gmail.com>
2024-07-04 08:58:44 +0300

    Chapter 3.3. Encoding Structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"greenlight.abdulalsh.com/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
+	//an instance of the movie struct with the id we got from the url and some dummy data, notice that we don't set a value for the year
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	//we will encode the struct to json and send it as http response
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The sevrer encounterd an error and couldn't process your request", http.StatusInternalServerError)
+	}
 	//otherwise, interpolate the movie id in a placeholder responose
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
