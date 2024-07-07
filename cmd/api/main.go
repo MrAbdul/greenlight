279b2d54988a0d18f2a06ab002ad21f2a4897bec
mrabdul <mrabdulrahman95@gmail.com>
2024-07-07 21:39:55 +0300

    Chapter 7.1. Setting up the Movie Model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	_ "github.com/lib/pq"
+	"greenlight.abdulalsh.com/internal/data"
 	"log/slog"
 	"net/http"
 	"os"
@@ -34,6 +35,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -75,6 +77,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
---
+++ internal/data/models.go
@@ -0,0 +1,27 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+//As an additional step, we’re going to wrap our MovieModel in a parent Models struct. Doing this is totally optional,
+//but it has the benefit of giving you a convenient single ‘container’ which can hold and represent all your database models as your application grows.
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+)
+
+// Create a Models struct which wraps the MovieModel. We'll add other models to this,
+// like a UserModel and PermissionModel, as our build progresses.
+type Models struct {
+	Movies MovieModel
+}
+
+// For ease of use, we also add a New() method which returns a Models struct containing
+// the initialized MovieModel.
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
