755cfa2a011a739d95fc0288fd8dbedaf5aa3ec5
mrabdul <mrabdulrahman95@gmail.com>
2024-07-07 15:25:41 +0300

    Chapter 4.5. Validating JSON Input

---
+++ internal/validator/validator.go
@@ -0,0 +1,44 @@
+package validator
+
+import (
+	"regexp"
+	"slices"
+)
+
+// regex matching for email checks
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+func (v *Validator) AddError(key string, msg string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = msg
+	}
+}
+func (v *Validator) Check(ok bool, key, mesage string) {
+	if !ok {
+		v.AddError(key, mesage)
+	}
+}
+func PermittedValues[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
+}
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+func Unique[T comparable](value []T) bool {
+	uniqValues := make(map[T]bool)
+	for _, v := range value {
+		uniqValues[v] = true
+	}
+	return len(value) == len(uniqValues)
+}
