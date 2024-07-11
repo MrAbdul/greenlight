9a4d09cb0ccd736eb41cfe241b1a6ea24028e8c9
abdulrahman <mrabdulrahman95@gmail.com>
2024-07-11 12:07:29 +0300

    Chapter 9.3. Listing Data

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -118,3 +118,36 @@ func (m MovieModel) Delete(id int64) error {
 	}
 	return nil
 }
+func (m MovieModel) GetAll(title string, generes []string, filters Filters) ([]*Movie, error) {
+	stmt := "SELECT id,created_at,title,year,runtime,genres,version FROM movies ORDER BY id"
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	rows, err := m.DB.QueryContext(ctx, stmt)
+	if err != nil {
+		return nil, err
+	}
+	//defer rows.close to ensure resultset is closed
+	defer rows.Close()
+	movies := []*Movie{}
+	for rows.Next() {
+		var movie Movie
+		//scan the value into the struct
+		err := rows.Scan(
+			&movie.ID,
+			&movie.CreatedAt,
+			&movie.Title,
+			&movie.Year,
+			&movie.Runtime,
+			pq.Array(&movie.Genres),
+			&movie.Version)
+		if err != nil {
+			return nil, err
+		}
+		movies = append(movies, &movie)
+	}
+	//when the rows next loop is done, call rows.err to retrive any errors
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return movies, nil
+}
