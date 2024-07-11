7662ce066d5298429947f34f5dbef95d68312ffe
abdulrahman <mrabdulrahman95@gmail.com>
2024-07-11 11:50:57 +0300

    Chapter 9.2. Validating Query String Parameters added validation to the filter params

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,21 @@
 package data
 
+import "greenlight.abdulalsh.com/internal/validator"
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+	//check the page, and page_size params contain sensible values
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "Must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+
+	//check that the sort parameter matches a value in the safelist
+	v.Check(validator.PermittedValues(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
