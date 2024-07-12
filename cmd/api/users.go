b0ecf3b0f81c009d071c8a05abbac8d1e595b2aa
mrabdul <mrabdulrahman95@gmail.com>
2024-07-12 14:01:17 +0300

    Chapter 13.4. Sending Background Emails we make use of goroutines to send emails in the backgrounds as to not affect the speed of the http connection. as the user doesn't have to wait for the email to be sent

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -52,16 +52,28 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
-	// Call the Send() method on our Mailer, passing in the user's email address,
-	// name of the template file, and the User struct containing the new user's data.
-	err = app.mailer.Send(user.Email, "user_welcome.gohtml", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+
+	// Launch a goroutine which runs an anonymous function that sends the welcome email.
+	go func() {
+		/*
+			The code running in the background goroutine forms a closure over the user and app variables.
+			It’s important to be aware that these ‘closed over’ variables are not scoped to the background goroutine,
+			which means that any changes you make to them will be reflected in the rest of your codebase.
+			For a simple example of this, see the following https://go.dev/play/p/eTz1xBm4W2a
+
+			In our case we aren’t changing the value of these variables in any way, so this behavior won’t cause us any issues. But it is important to keep in mind.
+		*/
+		err = app.mailer.Send(user.Email, "user_welcome.gohtml", user)
+		if err != nil {
+			// Importantly, if there is an error sending the email then we use the
+			// app.logger.Error() helper to manage it, instead of the
+			// app.serverErrorResponse() helper like before.
+			app.logger.Error(err.Error())
+		}
+	}()
 	// Write a JSON response containing the user data along with a 201 Created status
 	// code.
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
