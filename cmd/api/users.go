fe4046a67ab79c5d741141bab9db143e87d63c72
mrabdul <mrabdulrahman95@gmail.com>
2024-07-13 14:00:34 +0300

    Chapter 14.2. Creating Secure Activation Tokens

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -5,6 +5,7 @@ import (
 	"greenlight.abdulalsh.com/internal/data"
 	"greenlight.abdulalsh.com/internal/validator"
 	"net/http"
+	"time"
 )
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,11 +53,20 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+	token, err := app.models.Token.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
 	// Use the background helper to execute an anonymous function that sends the welcome
 	// email. which contains panic recovery
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.gohtml", user)
+		d := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.gohtml", d)
 		if err != nil {
 			// Importantly, if there is an error sending the email then we use the
 			// app.logger.Error() helper to manage it, instead of the
@@ -69,5 +79,6 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
+
 	}
 }
