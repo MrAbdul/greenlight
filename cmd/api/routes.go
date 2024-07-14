d8221e8310c45f60e8b53efe9db341313fbe4439
abdulrahman <mrabdulrahman95@gmail.com>
2024-07-14 12:31:11 +0300

    Chapter 16.1. Requiring User Activation

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -21,11 +21,11 @@ func (app *application) routes() http.Handler {
 	//static files
 	router.ServeFiles("/ui/*filepath", http.FS(ui.Files))
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requireActivateduser(app.createMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requireActivateduser(app.showMovieHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requireActivateduser(app.updateMovieHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requireActivateduser(app.deleteMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requireActivateduser(app.listMoviesHandler))
 
 	//users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
